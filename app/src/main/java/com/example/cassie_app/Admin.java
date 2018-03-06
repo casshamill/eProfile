df605eb83dafd9cc33438384ff2b87f6711e9276
Cassie Hamill <casshamill304@gmail.com>
2018-03-06 10:32:03 +0000

    Added new admin.java and updates  On branch master  Changes to be committed: 	new file:   app/src/main/java/com/example/cassie_app/Admin.java 	modified:   app/src/main/java/com/example/cassie_app/MainParentActivity.java

---
+++ app/src/main/java/com/example/cassie_app/Admin.java
@@ -0,0 +1,27 @@
+package com.example.cassie_app;
+
+public class Admin {
+    private String school_id;
+    private String email;
+
+    public Admin(String email, String school_id){
+        this.email = email;
+        this.school_id = school_id;
+    }
+
+    public String getSchool_id() {
+        return school_id;
+    }
+
+    public void setSchool_id(String school_id) {
+        this.school_id = school_id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
