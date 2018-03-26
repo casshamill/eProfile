00c272389066c73c1673e0383b5e963c3d41a9c2
Cassie Hamill <casshamill304@gmail.com>
2018-03-26 13:42:38 +0100

    Added new admin.java and updates  On branch master  Changes to be committed: 	new file:   app/src/main/java/com/example/cassie_app/Admin.java 	modified:   app/src/main/java/com/example/cassie_app/MainParentActivity.java

--- app/src/main/java/com/example/cassie_app/MainParentActivity.java
+++ app/src/main/java/com/example/cassie_app/MainParentActivity.java
@@ -1,13 +1,36 @@
 package com.example.cassie_app;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import com.example.cassie_app.R;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainParentActivity extends AppCompatActivity {
+
+    private DatabaseReference mDatabase;
+    private FirebaseAuth mAuth;
+    LinearLayout p;
+    List<Post> posts;
+    RecyclerView rv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +45,57 @@ public class MainParentActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mAuth = FirebaseAuth.getInstance();
+        findPupil();
+        rv = findViewById(R.id.rv);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        rv.setLayoutManager(llm);
+        posts = new ArrayList<>();
+    }
+
+    private void findPupil () {
+        DatabaseReference parent = mDatabase.child("parents").child( mAuth.getCurrentUser().getUid());
+        parent.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String pupil = (String)dataSnapshot.child("pupil_id").getValue();
+                loadData(pupil);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void loadData(String pupilid){
+        final TextView nameView = findViewById(R.id.pupil_name);
+        DatabaseReference pupil = mDatabase.child("pupils").child(pupilid);
+        pupil.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                DataSnapshot feed = dataSnapshot.child("feed");
+                String name = (String) dataSnapshot.child("name").getValue();
+                nameView.setText(name);
+                for (DataSnapshot p : feed.getChildren()) {
+                    String content = (String) p.child("content").getValue();
+                    String author = (String) p.child("author").getValue();
+                    System.out.println("Author:" + author + " -  Content : " + content);
+                    Post post = new Post(author, content, "11/03/2018");
+                    System.out.println("post" + post);
+                    posts.add(post);
+                }
+                RVAdapter adapter = new RVAdapter(posts);
+                rv.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
             }
         });
     }
