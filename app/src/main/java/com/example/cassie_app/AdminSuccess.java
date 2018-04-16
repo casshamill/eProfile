dfe3d45aab0bc1b24e1c0bae102c0ab48884b1e8
Cassie Hamill <casshamill304@gmail.com>
2018-04-16 19:54:20 +0100

    Adding more UI changes 	modified:   app/src/main/AndroidManifest.xml 	new file:   app/src/main/java/com/example/cassie_app/AdminSuccess.java 	new file:   app/src/main/res/layout/activity_admin_success.xml 	modified:   app/src/main/res/values/strings.xml

---
+++ app/src/main/java/com/example/cassie_app/AdminSuccess.java
@@ -0,0 +1,35 @@
+package com.example.cassie_app;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class AdminSuccess extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_success);
+
+        Button AdminHome = (Button) findViewById(R.id.returnadmin_btn);
+        Button TeacherHome = (Button) findViewById(R.id.returnhome_btn);
+
+        AdminHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(AdminSuccess.this, AdminTeacherActivity.class);
+                AdminSuccess.this.startActivity(i);
+            }
+        });
+
+        TeacherHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(AdminSuccess.this, MainTeacherActivity.class);
+                AdminSuccess.this.startActivity(i);
+            }
+        });
+    }
+}
