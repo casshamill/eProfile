e8b64e2f3e7241aac593035f4a0e407facdc357b
Cassie Hamill <casshamill304@gmail.com>
2018-04-16 18:59:59 +0100

    Update to SuccessPost 	modified:   app/src/main/java/com/example/cassie_app/successPost.java

--- app/src/main/java/com/example/cassie_app/successPost.java
+++ app/src/main/java/com/example/cassie_app/successPost.java
@@ -10,24 +10,10 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class successPost extends AppCompatActivity {
 
-    private FirebaseAuth mAuth;
-    var user = firebase.auth().currentUser;
-    var name, email, photoUrl, uid, emailVerified;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_success_post);
-
-        if (user != null) {
-            name = user.displayName;
-            email = user.email;
-            photoUrl = user.photoURL;
-            emailVerified = user.emailVerified;
-            uid = user.uid;  // The user's ID, unique to the Firebase project. Do NOT use
-            // this value to authenticate with your backend server, if
-            // you have one. Use User.getToken() instead.
-        }
 
         Button RegButton = (Button) findViewById(R.id.return_btn);
         RegButton.setOnClickListener(new View.OnClickListener() {
